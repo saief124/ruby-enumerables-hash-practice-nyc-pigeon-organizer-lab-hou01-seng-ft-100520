d105bec18b806e60daa11f3566062268f9770794
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-23 20:19:35 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+#require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+results=data.each_with_object({}) do |(k, v), a|
+  v.each do |color_key, pigeon_list|
+    pigeon_list.each do |pigeon_name|
+      if !a[pigeon_name]
+        a[pigeon_name]={}
+      end
+      if !a[pigeon_name][k]
+        !a[pigeon_name][k]=[]
+      end
+      a[pigeon_name][k].push(color_key.to_s)
+    end
+  end
+end
+results
+#binding.pry
+
+
+
+
+
 end
